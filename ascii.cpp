8208fabf3fd6ccabef927da4d901a6c542e0a58d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-02-25 23:30:29 -0700

    made another for loop but with a different range to iterate and display lowercased alphabet

--- ascii.cpp
+++ ascii.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main() {
         // capital A to capital Z
-    for (int i = 65; i <= 90; ++i) {
+    /* for (int i = 65; i <= 90; ++i) {
+        cout << (char) i << endl;
+    } */
+
+    for (int i = 97; i <= 122; ++i) {
         cout << (char) i << endl;
     }
 
