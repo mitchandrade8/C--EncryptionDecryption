d374ce2b51cc4a8070ae04ddcc0de2bfcbf70541
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-02-25 23:22:37 -0700

    going over ascii

---
+++ Vigenere.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string vigenere(string plainText, string key, bool EncDec);
+void caeser(string plainText, int key, bool EncDec);
+
+//#if 1
+int main() {
+    int option = 0;
+
+    string vKey;
+    bool again = 0;
+    char x;
+    string plainText;
+    string cipherText;
+
+    cout << "Welcome to Encryption-Decryption Station" << endl;
+
+
+    return 0;
+}
